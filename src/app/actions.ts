bf579e19ce7a8c7b34f73c6ae9f396699463a3a3
Alexander Almström <info@alexanderalmstrom.com>
2024-12-14 23:40:30 +0100

    Downgrade to next 14 and react 18

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -4,7 +4,20 @@ type FormState = {
   message: string;
 };
 
-export async function exampleAction(prevState: FormState, formData: FormData) {
+export async function exampleFormActionState(
+  prevState: FormState,
+  formData: FormData
+) {
+  const name = formData.get("name");
+
+  if (!name || typeof name !== "string") {
+    return { message: "VALIDATION_ERROR" };
+  }
+
+  return { message: `Hello, ${name}!` };
+}
+
+export async function exampleAction(formData: FormData) {
   const name = formData.get("name");
 
   if (!name || typeof name !== "string") {
