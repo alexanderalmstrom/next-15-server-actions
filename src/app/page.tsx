3fd6dbf2bd79d26476bcbf9f6d1a2419d9cfbad6
Alexander Almström <info@alexanderalmstrom.com>
2024-12-15 00:05:19 +0100

    Remove the form with action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 import FormWithAction from "./components/FormWithAction";
-import FormWithActionState from "./components/FormWithActionState";
 
 export default function Home() {
-  return (
-    <>
-      <FormWithActionState />
-      <FormWithAction />
-    </>
-  );
+  return <FormWithAction />;
 }
