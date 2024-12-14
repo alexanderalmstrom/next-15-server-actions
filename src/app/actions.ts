4d842ec261ffbf3a592701321eb7a221b0e6759c
Alexander Almström <info@alexanderalmstrom.com>
2024-12-15 00:06:07 +0100

    Remove example action

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,22 +1,5 @@
 "use server";
 
-type FormState = {
-  message: string;
-};
-
-export async function exampleFormActionState(
-  prevState: FormState,
-  formData: FormData
-) {
-  const name = formData.get("name");
-
-  if (!name || typeof name !== "string") {
-    return { message: "VALIDATION_ERROR" };
-  }
-
-  return { message: `Hello, ${name}!` };
-}
-
 export async function exampleAction(formData: FormData) {
   const name = formData.get("name");
 
